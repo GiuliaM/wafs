aeea253dfb5b9176812af4524140f128b33cfd4e
GiuliaM <Giulia@057.225.028.145.hva.nl>
2017-02-14 12:12:39 +0100

    Feedback changes

--- opdracht6-api/static/app.js
+++ opdracht6-api/static/app.js
@@ -7,15 +7,38 @@
 
 // Name=Eric&Age=26
 
+//
+//var apiKey= "dd38c2386787f64e844c36f6dec9c600"; // Dit is de api key van petfinder
+//var apiSecret= "ee1fcad95b967700d452473a32196b96"
 
-var apiKey= "dd38c2386787f64e844c36f6dec9c600"; // Dit is de api key van petfinder
+
+var config = {
+    apiKey:'#',
+    apiSecret:'#',
+    apiUrl: 'http://dennistel.nl/movies'
+};
+
+var render = {
+    toggle: function(data) {
+        console.log(data);
+    }
+
+};
 
 var xhr= new XMLHttpRequest();
-xhr.open("GET", "https://www.codecademy.com/", false); // GET is het aanvragen van info, de url en false betekent dat de functie wacht op reactie van de server
+xhr.open('GET', 'config.apiUrl', false); // GET: aanvragen van info, false betekent dat de functie wacht op reactie van de server
 //the exercise will wait until it gets a response from the server
 
 xhr.send();
 
-console.log(xhr.status);
-console.log(xhr.statusText);
+//console.log(xhr.status);
+//console.log(xhr.statusText);
 
+xhr.onreadystatechange = function() {
+if (readyState === 4) {
+    if (req.status === 200 || req.status === 201) {
+        succes(req.responseText);
+    }
+
+}
+};
