cd6d082b404dbd6f67391d7cd5a3b11535707ca6
GiuliaM <Giulia@057.225.028.145.hva.nl>
2017-02-14 15:10:47 +0100

    v. 1.0

--- opdracht6-api/static/app.js
+++ opdracht6-api/static/app.js
@@ -1,44 +1,49 @@
-/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+///*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 
-
-// POST /codecademy/learn-http HTTP/1.1
-// Host: www.codecademy.com
-// Content-Type: text/html; charset=UTF-8
-
-// Name=Eric&Age=26
-
-//
-//var apiKey= "dd38c2386787f64e844c36f6dec9c600"; // Dit is de api key van petfinder
-//var apiSecret= "ee1fcad95b967700d452473a32196b96"
-
+(function() {
+    'use strict';
 
 var config = {
-    apiKey:'#',
-    apiSecret:'#',
+//    apiKey:'#',
+//    apiSecret:'#',
     apiUrl: 'http://dennistel.nl/movies'
-};
-
-var render = {
-    toggle: function(data) {
-        console.log(data);
-    }
-
-};
-
-var xhr= new XMLHttpRequest();
-xhr.open('GET', 'config.apiUrl', false); // GET: aanvragen van info, false betekent dat de functie wacht op reactie van de server
-//the exercise will wait until it gets a response from the server
-
-xhr.send();
-
-//console.log(xhr.status);
-//console.log(xhr.statusText);
-
-xhr.onreadystatechange = function() {
-if (readyState === 4) {
-    if (req.status === 200 || req.status === 201) {
-        succes(req.responseText);
-    }
-
 }
-};
+
+var localStorage, table = [];
+
+//aja is the mini library.  With .url you tell where to get the info.  With .on you say: if successfull load the data in function(data). In function data I made the object newEntry which states the function saveData(). Which pushes the data to the array table.
+aja()
+    .url(config.apiUrl)
+    .on('success', function(data) {
+        localStorage = {
+          saveData: function(){
+              table.push(data);
+              console.log(table);
+          }
+        }
+        localStorage.saveData()
+    })
+    .go();
+
+
+
+
+    //    function getUserInput() {
+//      var userInput = document.getElementById("user-input-field").value;
+//      console.log(userInput);
+//    }
+
+}());
+
+
+
+
+
+
+
+
+
+
+
+
+
